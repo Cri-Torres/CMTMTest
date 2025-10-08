2ab551c1d2d86874c8acd33040b685b76639c40e
Cristian Torres <miguel29torres12@gmail.com>
2025-10-08 09:45:46 -0600

    Creacion de la clase Texto

---
+++ CMTM.Texto/Texto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMTM.Texto
+{
+    public class Texto
+    {
+        // Método que invierte la cadena
+        public string ReverseString(string input)
+        {
+            if (input == null) return null;
+
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        // Método que elimina todos los espacios en blanco
+        public string RemoveSpaces(string input)
+        {
+            if (input == null) return null;
+
+            return input.Replace(" ", "");
+        }
+    }
+}
