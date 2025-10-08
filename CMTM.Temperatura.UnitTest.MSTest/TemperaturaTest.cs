8f58962d8b2a628f0cf23a7c42103b1197198738
Cristian Torres <miguel29torres12@gmail.com>
2025-10-08 09:35:18 -0600

    Creacion de la clase test de temperatura

---
+++ CMTM.Temperatura.UnitTest.MSTest/TemperaturaTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMTM.Temperatura.UnitTest.MSTest
+{
+    [TestClass]
+    public class TemperaturaTest
+    {
+        [TestMethod]
+        public void CelsiusToFahrenheit_25C_Retorna77F()
+        {
+            // Arrange
+            var temp = new Temperatura();
+            double celsius = 25;
+
+            // Act
+            double resultado = temp.CelsiusToFahrenheit(celsius);
+
+            // Assert
+            Assert.AreEqual(77, resultado, 0.001, "25°C debería ser 77°F");
+        }
+
+        [TestMethod]
+        public void FahrenheitToCelsius_77F_Retorna25C()
+        {
+            // Arrange
+            var temp = new Temperatura();
+            double fahrenheit = 77;
+
+            // Act
+            double resultado = temp.FahrenheitToCelsius(fahrenheit);
+
+            // Assert
+            Assert.AreEqual(25, resultado, 0.001, "77°F debería ser 25°C");
+        }
+
+        [TestMethod]
+        public void CelsiusToFahrenheit_0C_Retorna32F()
+        {
+            var temp = new Temperatura();
+            double resultado = temp.CelsiusToFahrenheit(0);
+            Assert.AreEqual(32, resultado, 0.001);
+        }
+
+        [TestMethod]
+        public void FahrenheitToCelsius_32F_Retorna0C()
+        {
+            var temp = new Temperatura();
+            double resultado = temp.FahrenheitToCelsius(32);
+            Assert.AreEqual(0, resultado, 0.001);
+        }
+    }
+}
--- CMTM.Temperatura/Temperatura.cs
+++ CMTM.Temperatura/Temperatura.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CMTM.Temperatura
 {
-    internal class Temperatura
+    public class Temperatura
     {
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
     }
 }
